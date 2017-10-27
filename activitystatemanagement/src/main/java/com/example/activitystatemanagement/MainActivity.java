bd7545c5b2ee2f871c72c1833b8b2cc2ddbea885
sonalteke <sonal.teke11@gmail.com>
2017-10-27 15:19:43 +0530

    Alternative Resources 1

---
+++ activitystatemanagement/src/main/java/com/example/activitystatemanagement/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.activitystatemanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState!=null){
+            My my=(My) savedInstanceState.getSerializable("myDt");
+            ((TextView)findViewById(R.id.txtVw)).setText(my.dt);
+        }
+        mt("onCreate");
+    }
+
+    @Override
+    protected void onDestroy() {
+        mt("onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        My my=new My();
+        my.dt=((TextView)findViewById(R.id.txtVw)).getText().toString();
+
+        outState.putSerializable("mtDt",my);
+    }
+
+    public void myClick(View view){
+        ((TextView)findViewById(R.id.txtVw)).setText(new Date().toString());
+    }
+
+    private void mt(String msg){
+        Toast.makeText(this, "msg", Toast.LENGTH_SHORT).show();
+    }
+
+    class My implements Serializable{
+        String dt;
+    }
+}
